7ebdc49cca61f8a765ea7258aec08c7b60a39b6b
amineketa <amineketa2001@gmail.com>
2023-11-25 14:51:26 +0100

    wip

--- src/Components/SearchDiv/Search.jsx
+++ src/Components/SearchDiv/Search.jsx
@@ -2,18 +2,27 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import { BsHouseDoor } from "react-icons/bs";
 import { CiLocationOn } from "react-icons/ci";
+import { useState } from "react";
 
 const Search = () => {
+  const [search, setSearch] = useState("");
+
+  const changeHandler = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
-    <div className="searchDive grid gap-10 bg-[#f1f4f8] rounded-[10px] p-[48px]">
-       <p className=" text-gray-500 text-[56px] font-medium font-['Inter'] leading-[64px] ">
-                Find a job that suits <br></br>your interest & skills.
-              </p>
+    <div className="searchDive grid gap-10 bg-[#f1f4f8] rounded-[10px] p-[48px] ">
+      <p className=" text-gray-500 text-[56px] font-medium font-['Inter'] leading-[64px] ">
+        Find a job that suits <br></br>your interest & skills.
+      </p>
       <form action="">
         <div className="firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-[#f1f4f8 700]">
           <div className="flex gap-2 items-center">
             <AiOutlineSearch className="text-[25px] icon" />
             <input
+              value={search}
+              onChange={changeHandler}
               type="text"
               className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
               placeholder="Search Job Here...."
@@ -39,7 +48,19 @@ const Search = () => {
             <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-[#252b36] icon" />
           </div>
 
-          <button className="bg-[#2a68ff] h-full p-5 px-10 rounded-[10px] text-white cursor-pointer hover:bg-blue-300">
+          <button
+            onClick={async (event) => {
+              event.preventDefault();
+              console.log(search);
+              //Make request to search
+              const response = await fetch(`http://localhost:8000/api/jobs?query=${search}`, {
+                method: 'GET',
+              });
+              const jobs = await response.json();
+              console.log(jobs);
+            }}
+            className="bg-[#2a68ff] h-full p-5 px-10 rounded-[10px] text-white cursor-pointer hover:bg-blue-300"
+          >
             Search
           </button>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>,
+        loader: async () => {
+          const response = await fetch(`http://localhost:8000/api/jobs`, {
+                method: 'GET',
+              });
+              const jobs = await response.json();
+              return jobs;
+        }
+      }
+    ]
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes}/>
   </React.StrictMode>,
 )
