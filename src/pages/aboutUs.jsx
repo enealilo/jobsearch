cd3d6a9e6395508e0da5b506d8df02ac2ed05515
User <enealilo1996@gmail.com>
2023-12-02 15:39:57 +0100

    about_us

--- src/pages/aboutUs.jsx
+++ src/pages/aboutUs.jsx
@@ -1,40 +1,183 @@
-export default function AboutUs()
-{
-    return (
-        <>
-        <h1 className="font-bold ">About Us</h1>
-      <h3>Welcome to ByteCareer, where we bridge the gap between talent and opportunity in the dynamic world of technology. Our platform is designed to empower both job seekers and employers, creating a seamless and efficient connection that propels the tech industry forward. </h3>
-
-<p>
-Our Mission
-
-At ByteCareer, we are driven by a singular mission: to match exceptional talent with the right opportunities in the ever-evolving landscape of technology. We believe that the right job can transform lives and drive innovation, and we are committed to facilitating those transformative connections.
-
-What Sets Us Apart
-
-Precision Matching: Our advanced algorithms and machine learning capabilities ensure precise job matches, considering not only skills and experience but also cultural fit and career aspirations.
-
-Comprehensive Tech Spectrum: Whether you are a seasoned software engineer, a cybersecurity expert, a data scientist, or exploring emerging tech fields, [Your Tech Job Finder] covers a broad spectrum of technology roles across industries.
-
-User-Centric Design: Navigating the job market can be overwhelming. Our user-friendly interface simplifies the process, providing an intuitive and enjoyable experience for both job seekers and employers.
-
-Industry Insights: Stay ahead of the curve with our curated content, featuring industry trends, career advice, and insights from tech leaders. We believe that informed professionals make better career decisions.
-
-For Job Seekers
-
-Embark on your tech journey with confidence. ByteCareer offers a platform where you can explore exciting career opportunities, access valuable resources, and connect with top-tier employers actively seeking your skills.
-
-For Employers
-
-Source top tech talent effortlessly. Our platform streamlines the hiring process, allowing you to find the perfect match for your team quickly. Post jobs, review applications, and connect with qualified candidates seamlessly.
-
-Join Us in Shaping the Future
-
-Whether you're a seasoned professional or an ambitious newcomer to the tech industry, [Your Tech Job Finder] is your partner in navigating the job market. Together, let's shape the future of technology by fostering connections that drive innovation and propel careers to new heights.
-
-Discover your next tech adventure with ByteCareer – where opportunities meet expertise, and careers are transformed.
-        </p>
-
-        </>
-    )
+export default function AboutUs() {
+  return (
+    <>
+      <div className="w-[1439px] h-[536px] px-[15px] bg-white justify-start items-center inline-flex">
+        <div className="grow shrink basis-0 self-stretch relative">
+          <div className="w-[300.43px] h-[536px] left-0 top-0 absolute rounded-bl-[250px] rounded-br-[250px] flex-col justify-end items-center gap-2.5 inline-flex">
+            <img
+              className="w-[295px] h-[295px] rounded-[250px]"
+              src="/handshake.jpg"
+            />
+          </div>
+          <div className="w-[1109px] h-[391px] left-[300px] top-0 absolute bg-[#f1f4f8] rounded-br-[7px]">
+            <div className="left-[183px] top-[106px] absolute text-center text-fuchsia-800 text-[40px] font-bold font-['Koulen'] leading-[50px]">
+              WHO WE ARE?
+            </div>
+            <div className="w-[743px] left-[182.79px] top-[173px] absolute text-fuchsia-800 text-lg font-normal font-['Lexend'] leading-7">
+              Welcome to ByteCareer, where we bridge the gap between talent and
+              opportunity in the dynamic world of technology. Our platform is
+              designed to empower both job seekers and employers, creating a
+              seamless and efficient connection that propels the tech industry
+              forward.{" "}
+            </div>
+            <div className="py-2.5 left-[182.79px] top-[302px] absolute justify-start items-center gap-2.5 inline-flex">
+              <div className="w-6 h-6 relative"></div>
+              <div className="w-6 h-6 relative"></div>
+              <div className="justify-center items-center gap-2.5 flex"></div>
+            </div>
+          </div>
+          <div className="w-[200px] h-[200px] p-2.5 left-[175px] top-[75px] absolute bg-[#2a68ff] rounded-[500px] border border-indigo-50 justify-center items-center gap-2.5 inline-flex">
+            <div className="text-center text-white text-[32px] font-normal font-['Koulen'] leading-[50px]">
+              About Us
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-[1436px] h-[566px] px-[15px] bg-white justify-start items-center inline-flex">
+        <div className="w-[894px] h-[393px] px-[30px] py-2.5 flex-col justify-center items-start gap-2.5 inline-flex">
+          <div className="self-stretch border-b border-red-200 justify-start items-start gap-2.5 inline-flex">
+            <div className="grow shrink basis-0 text-fuchsia-800 text-[32px] font-normal font-['Koulen'] leading-[50px]">
+              OUR MISSION
+            </div>
+          </div>
+          <div className="self-stretch text-black text-lg font-light font-['Lexend Deca'] leading-7">
+            We are driven by a singular mission: to match exceptional talent
+            with the right opportunities in the ever-evolving landscape of
+            technology. We believe that the right job can transform lives and
+            drive innovation, and we are committed to facilitating those
+            transformative connections. What Sets Us Apart Precision Matching:
+            Our advanced algorithms and machine learning capabilities ensure
+            precise job matches, considering not only skills and experience but
+            also cultural fit and career aspirations. Comprehensive Tech
+            Spectrum: Whether you are a seasoned software engineer, a
+            cybersecurity expert, a data scientist, or exploring emerging tech
+            fields, covers a broad spectrum of technology roles across
+            industries. User-Centric Design: Navigating the job market can be
+            overwhelming. Our user-friendly interface simplifies the process,
+            providing an intuitive and enjoyable experience for both job seekers
+            and employers. Industry Insights: Stay ahead of the curve with our
+            curated content, featuring industry trends, career advice, and
+            insights from tech leaders. We believe that informed professionals
+            make better career decisions.
+          </div>
+        </div>
+        <div className="grow shrink basis-0 rounded-[400px] flex-col justify-center items-center gap-2.5 inline-flex">
+          <div className="w-[503px] h-[566px] relative">
+            <div className="w-[379.56px] h-[380.53px] left-[124.01px] top-0 absolute bg-[#2a68ff] rounded-full"></div>
+            <img
+              className="w-[475.66px] h-[475.66px] left-0 top-[71.84px] absolute rounded-[500px] shadow border-4 bg-[#2a68ff]"
+              src="/digital.jpg"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="w-[1436px] pb-2.5 justify-center items-end inline-flex">
+        <div className="w-[620px] self-stretch justify-start items-start gap-2.5 inline-flex">
+          <div className="w-[284px] h-[604px] px-[34px] flex-col justify-center items-center gap-5 inline-flex">
+            <div className="w-[250px] h-[291px] relative">
+              <img
+                className="w-[250px] h-[250px] left-0 top-0 absolute rounded-[250px] border-4 border-[#AED6F1]"
+                src="/rudina.jpg"
+              />
+              <div className="p-[5px] left-0 top-[111px] absolute opacity-90 bg-[#AED6F1] rounded-[5px] justify-center items-center gap-2.5 inline-flex">
+                <div className="text-center text-black text-base font-light font-['Inter']">
+                  Rudina Himallari
+                </div>
+              </div>
+            </div>
+            <div className="w-[250px] h-[291px] relative">
+              <img
+                className="w-[250px] h-[250px] left-0 top-0 absolute rounded-[250px] border-4 border-[#AED6F1]"
+                src="/enea.JPG"
+              />
+              <div className="px-2.5 py-[5px] left-0 top-[111px] absolute opacity-90 bg-[#AED6F1] rounded-[5px] justify-center items-center gap-2.5 inline-flex">
+                <div className="text-center text-black text-base font-light font-['Inter']">
+                  Enea Lilo
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-[323px] px-2.5 justify-start items-center gap-2.5 flex">
+            <div className="w-[303px] h-96 relative">
+              <div className="w-[273px] h-[273px] left-[15px] top-[29px] absolute rounded-full shadow" />
+              <div className="w-[250px] h-[251px] left-0 top-[80px] absolute">
+                <img
+                  className="w-[250px] h-[250px] left-0 top-[0.50px] absolute rounded-[250px] border-4 border-[#AED6F1]"
+                  src="/amine.jpg"
+                />
+                <div className="px-2.5 py-[5px] left-0 top-[111px] absolute opacity-90 bg-[#AED6F1] rounded-[5px] justify-center items-center gap-2.5 inline-flex">
+                  <div className="text-center text- text-base font-light font-['Inter']">
+                    Amine Keta
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-[816px] self-stretch p-[15px] flex-col justify-start items-start gap-2.5 inline-flex">
+          <div className="justify-start items-start gap-2.5 inline-flex">
+            <div className="text-black text-[32px] font-normal font-['Koulen'] leading-[50px]">
+              The Teams
+            </div>
+          </div>
+          <div className="self-stretch  px-[15px] justify-start items-center gap-2.5 inline-flex">
+            <div className="grow shrink basis-0 self-stretch">
+              <span className="text-black text-xxlg font-normal font-['Lexend'] leading-7">
+                1:{" "}
+              </span>
+              <span className="text-black text-[25px] font-bold font-['Lexend'] leading-7">
+                Rudina Himallari
+              </span>
+              <span className="text-black text-lg font-normal font-['Lexend'] leading-7">
+                {" "}
+              </span>
+              <span className="text-black text-lg font-normal font-['Lexend'] leading-7">
+                Experienced web developer adept at translating creative concepts
+                into robust, responsive websites. Proficient in front-end
+                technologies such as HTML, CSS, and JavaScript, I specialize in
+                crafting visually appealing and user-friendly interfaces. With a
+                keen eye for detail, I ensure cross-browser compatibility and
+                optimal performance for a seamless user experience. My
+                collaborative approach involves working closely with design and
+                backend teams to deliver cohesive and scalable solutions.
+                Committed to staying abreast of industry trends, I bring a
+                passion for innovation to every project, striving to create
+                impactful and modern web experiences <br />
+                <br />
+                2:{" "}
+              </span>
+              <span className="text-black text-[24px] font-bold font-['Lexend'] leading-7">
+                Enea Lilo
+              </span>
+              <span className="text-black text-lg font-bold font-['Lexend'] leading-7">
+                {" "}
+              </span>
+              <span className="text-black text-lg font-normal font-['Lexend'] leading-7">
+                As a web developer, I specialize in transforming ideas into
+                robust, user-centric websites. With expertise in HTML, CSS, and
+                JavaScript, I create visually stunning and responsive designs
+                that elevate user experiences. My commitment to clean and
+                efficient code ensures optimal website performance and easy
+                maintenance. Experienced in collaborating with diverse teams, I
+                bring a collaborative and communicative approach to every
+                project. Constantly evolving with the evolving web landscape, I
+                stay updated on the latest technologies to deliver cutting-edge
+                solutions.
+                <br />
+                <br />
+                3:{" "}
+              </span>
+              <span className="text-black text-[24px] font-bold font-['Lexend'] leading-7">
+                Amine Keta
+              </span>
+              <span className="text-black text-lg font-normal font-['Lexend'] leading-7">
+                {" "}
+                "Detail-oriented web developer with a passion for crafting pixel-perfect, user-friendly websites. Proficient in the latest front-end technologies, including HTML5, CSS3, and JavaScript, I specialize in creating visually compelling and interactive online experiences. With a focus on responsive design, accessibility, and cross-browser compatibility, I ensure that websites function seamlessly across various platforms. Known for my problem-solving skills and ability to adapt to evolving technologies, I am dedicated to delivering high-quality, scalable solutions. Whether working independently or collaboratively, I bring a creative mindset and a commitment to staying ahead in the ever-evolving field of web development."{" "}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
